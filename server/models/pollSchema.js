72e2a8b9e6aad8599af4ad84b619e9662b7f4e76
danieljoonlee <danieljoonlee@gmail.com>
2016-04-20 14:46:10 -0700

    add routes, polls route can get id for mongoose model, polls route can create mongoose model

--- server/models/pollSchema.js
+++ server/models/pollSchema.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-//const QuestionSchema = require('./questionSchema');
 
-const pollSchema = new Schema({
+const pollSchema = mongoose.Schema({
 	// push array of question objects into schema
 	// id automaticaly generated
-	question: {type: Schema.Types.Mixed, required: true}
+	questions: {type: Schema.Types.Mixed, required: true}
 });
 
-const Poll = mongoose.model('Poll', pollSchema);
-
-module.export = Poll;
+module.exports = mongoose.model('Poll', pollSchema);
